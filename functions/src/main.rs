ec0f91cfe4be3f20a4c31aa7d8a2598c8fbe1454
= <=>
2023-05-27 16:30:04 +0200

    unit label

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     println!("Hello, world!");
-    another_function(5);
+    another_function(5, 'h');
 }
 
-fn another_function(x: i32) {
-    println!("The value is {x}");
+fn another_function(x: i32, label: char) {
+    println!("The value is {x}{label}");
 }
